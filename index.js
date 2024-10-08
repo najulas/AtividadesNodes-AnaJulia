ca15c23eb8dd4ebb0e6bc478e83e671250a29add
najulas <133673937+najulas@users.noreply.github.com>
2024-10-08 23:09:39 +0000

    08/10/24

--- index.js
+++ index.js
@@ -6,42 +6,58 @@ const anoNascimento = 2007;
 let anoAtual = 2024;
 const idade = 16;
 
-console.log("Olá eu sou " + nomeCompleto + " estamos no ano de " + anoAtual + " minha idade é " + idade + ".");
-
-let AnoAtual = 2025;
-let Idade = 17;
+let AnoAtual = 2024;
+let Idade = 16;
 const NomeCompleto = "Ana Julia Meira dos Santos";
-const Faculdade = "Engenharia Civil";
-
-console.log("Olá meu nome é " + NomeCompleto + ", tenho " + Idade + " anos " +  "estamos no ano de " + AnoAtual + " gostaria de fazer faculdade na área de " + Faculdade);
-
-console.log(`oi eu sou a ${nomeCompleto}, tenho ${idade} anos, nasci em ${anoNascimento}, e em ${AnoAtual} eu quero seguir na área de ${Faculdade}`);
+const Faculdade = "Engenharia";
+console.log("Olá meu nome é " + NomeCompleto + ", tenho " + Idade + " anos, " +  "estamos no ano de " + AnoAtual + " eu gostaria de fazer faculdade na área de " + Faculdade);
 
 const listaDeViagens = new Array(
-  `Japão`,
-  `Canadá`,
-  `Estados Unidos`,
-  `México`,
-  `Rússia`
-)
+`Japão` ,
+`México` ,
+`Canadá` ,
+`Estados Unidos` ,
+  )
 
 console.log(listaDeViagens);
 
-console.log(listaDeViagens[4]);
-listaDeViagens.push("Campo Mourão");
+console.log(listaDeViagens[3]);
+listaDeViagens.push("Coronel Vivida");
 console.log(listaDeViagens);
 
 listaDeViagens.splice(1,2);
 console.log(listaDeViagens);
 
-var nomeComprador = entrada("Qual é o seu nome? ")
+const loginCerto = "Naju";
+const senhaCerta = 12345;
+console.log("");
+console.log("                              🛫 AGÊNCIA DE VIAGENS 🛬");
+console.log("");
+console.log("Venha curtir suas férias conosco. Há mais de 40 anos transformando suas viagens em experiências inesquecíveis.");
+console.log(" ");
+console.log("Faça seu login");
+
+var login = entrada("Usuário ou endereço de email:")
+var senha = entrada("Bem vindo(a) novamente! Digite sua senha:")
+
+while(login != loginCerto){
+  console.log("Login Incorreto");
+  login = entrada("Digite seu login novamente: ");
+}
+
+while(senha != senhaCerta){
+  console.log(" Atenção! Senha incorreta!");
+  senha = entrada("Digite sua senha novamente: ");
+}
+
+var nomeComprador = entrada("Qual é o seu nome? ");
 var idadeComprador = entrada("Qual é a sua idade? ");
 
-if(idadeComprador < 18 ){
-  
-  console.log(`Olá ${nomeComprador}, não podemos prosseguir com a compra pois você tem ${idadeComprador} anos, é proibida a venda de passagens para menores de 18 anos.`)
+if (idadeComprador < 18) {
+
+  console.log(`Olá ${nomeComprador}! Não vendemos passagens para menores de 18 anos, já que a sua idade é ${idadeComprador} anos não iremos prosseguir com a compra.`);
 }
 
-if(idadeComprador >= 18){
-   console.log("Compra realizada com sucesso. Os destinos disponíveis são: México, Estados Unidos e Canadá. Obrigada pela preferência.")
-}
+if (idadeComprador >= 18) {
+console.log(`Bem vindo(a) ${nomeComprador}! Dê inicio a sua compra vericando os destinos disponiveís desta semana: Japão, México, Canadá e Estados Unidos`)
+  }
